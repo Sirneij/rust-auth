dc9aabc9a688ca222451a42332792d19a093a219
John Idogun <sirneij@gmail.com>
2023-04-23 20:52:49 +0100

    Database and redis configurations added

---
+++ backend/migrations/20230423180653_users_table.down.sql
@@ -0,0 +1,3 @@
+-- Add down migration script here
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS user_profile;
--- backend/src/startup.rs
+++ backend/src/startup.rs
@@ -4,7 +4,21 @@ pub struct Application {
 }
 
 impl Application {
-    pub async fn build(settings: crate::settings::Settings) -> Result<Self, std::io::Error> {
+    pub async fn build(
+        settings: crate::settings::Settings,
+        test_pool: Option<sqlx::postgres::PgPool>,
+    ) -> Result<Self, std::io::Error> {
+        let connection_pool = if let Some(pool) = test_pool {
+            pool
+        } else {
+            get_connection_pool(&settings.database).await
+        };
+
+        sqlx::migrate!()
+            .run(&connection_pool)
+            .await
+            .expect("Failed to migrate the database.");
+
         let address = format!(
             "{}:{}",
             settings.application.host, settings.application.port
@@ -12,7 +26,7 @@ impl Application {
 
         let listener = std::net::TcpListener::bind(&address)?;
         let port = listener.local_addr().unwrap().port();
-        let server = run(listener).await?;
+        let server = run(listener, connection_pool, settings).await?;
 
         Ok(Self { port, server })
     }
@@ -26,9 +40,36 @@ impl Application {
     }
 }
 
-async fn run(listener: std::net::TcpListener) -> Result<actix_web::dev::Server, std::io::Error> {
+pub async fn get_connection_pool(
+    settings: &crate::settings::DatabaseSettings,
+) -> sqlx::postgres::PgPool {
+    sqlx::postgres::PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(settings.connect_to_db())
+}
+
+async fn run(
+    listener: std::net::TcpListener,
+    db_pool: sqlx::postgres::PgPool,
+    settings: crate::settings::Settings,
+) -> Result<actix_web::dev::Server, std::io::Error> {
+    // Database connection pool application state
+    let pool = actix_web::web::Data::new(db_pool);
+
+    // Redis connection pool
+    let cfg = deadpool_redis::Config::from_url(settings.clone().redis.uri);
+    let redis_pool = cfg
+        .create_pool(Some(deadpool_redis::Runtime::Tokio1))
+        .expect("Cannot create deadpool redis.");
+    let redis_pool_data = actix_web::web::Data::new(redis_pool);
+
     let server = actix_web::HttpServer::new(move || {
-        actix_web::App::new().service(crate::routes::health_check)
+        actix_web::App::new()
+            .service(crate::routes::health_check)
+            // Add database pool to application state
+            .app_data(pool.clone())
+            // Add redis pool to application state
+            .app_data(redis_pool_data.clone())
     })
     .listen(listener)?
     .run();
