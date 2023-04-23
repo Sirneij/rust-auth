2cde46be5c577193e7739f22a5e169a423fd7a83
John Idogun <sirneij@gmail.com>
2023-04-23 08:34:09 +0100

    Production grade setup for rust backend services

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,2 +1,4 @@
 pub mod routes;
+pub mod settings;
 pub mod startup;
+pub mod telemetry;
---
+++ backend/src/telemetry.rs
@@ -0,0 +1,31 @@
+use tracing_subscriber::layer::SubscriberExt;
+
+pub fn get_subscriber(debug: bool) -> impl tracing::Subscriber + Send + Sync {
+    let env_filter = if debug {
+        "trace".to_string()
+    } else {
+        "info".to_string()
+    };
+    let env_filter = tracing_subscriber::EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new(env_filter));
+
+    let stdout_log = tracing_subscriber::fmt::layer().pretty();
+    let subscriber = tracing_subscriber::Registry::default()
+        .with(env_filter)
+        .with(stdout_log);
+
+    let json_log = if !debug {
+        let json_log = tracing_subscriber::fmt::layer().json();
+        Some(json_log)
+    } else {
+        None
+    };
+
+    let subscriber = subscriber.with(json_log);
+
+    subscriber
+}
+
+pub fn init_subscriber(subscriber: impl tracing::Subscriber + Send + Sync) {
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
+}
