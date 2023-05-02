4ac569fff79c0b540c9f25c1769c62b8fbeb6e14
John Idogun <sirneij@gmail.com>
2023-05-02 20:15:16 +0100

    Enhancing the frontend app

--- frontend/src/lib/utils/constant.ts
+++ frontend/src/lib/utils/constant.ts
@@ -47,11 +47,6 @@ export const topics: Array<Topic> = [
 	},
 	{
 		id: crypto.randomUUID(),
-		title: 'Log in/out, Dockerize and Deploy on fly.io',
-		url: 'https://dev.to/sirneij/authentication-system-using-rust-actix-web-and-sveltekit-log-inout-dockerize-and-deploy-on-flyio-58pc'
-	},
-	{
-		id: crypto.randomUUID(),
 		title: 'File upload to AWS S3, Profile Update',
 		url: 'https://dev.to/sirneij/authentication-system-using-rust-actix-web-and-sveltekit-file-upload-to-aws-s3-profile-update-3b2a'
 	},
